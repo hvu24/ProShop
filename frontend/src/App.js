de70cac4b224e6a798b5a1501a7cc0e7aacfb4cb
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-13 14:39:41 -0700

    rendered product list on home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import Header from './components/Header.js';
 import Footer from './components/Footer';
 
 import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen.js';
 
 function App() {
   return (
     <div>
       <Header />
       <main className='py-3'>
-        <Container><h1>Welcome</h1></Container>
+        <Container>
+          <HomeScreen />
+        </Container>
       </main>
       <Footer />
     </div>
