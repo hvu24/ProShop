775a99004e14f5335e6adb3c13ca9281daf9cf42
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-13 13:56:26 -0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import Header from './components/Header.js';
+import Footer from './components/Footer';
+
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container><h1>Welcome</h1></Container>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
