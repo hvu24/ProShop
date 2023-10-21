e0e223073838c44ecedaf71a9823e7c263ceb71f
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-20 19:16:27 -0700

    added get users route and route protection

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
+from .models import Product, User
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -45,11 +46,18 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
