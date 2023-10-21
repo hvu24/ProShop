cea079318111206360c0c30874ea860d8c5fd850
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-21 16:32:17 -0700

    reorganized views and urls

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .products import products
+from base.products import products
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product, User
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.models import Product, User
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -61,18 +61,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
