1fe6d148b33a2f87f4d27706ca45dac1fd9af549
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-21 20:13:33 -0700

    added user login form and function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
 import CartScreen from './screens/CartScreen.js';
+import LoginScreen from './screens/LoginScreen.js';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen/>} exact />
+            <Route path='/login' element={<LoginScreen/>} exact />
             <Route path='/product/:id' element={<ProductScreen/>} />
             <Route path='/cart/:id?' element={<CartScreen/>} />
             {/* ? makes the id optional */}
